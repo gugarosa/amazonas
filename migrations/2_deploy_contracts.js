f942ae7a421782765a45950c70e3d2ccaacd05ca
Gustavo Rosa <gth.rosa@uol.com.br>
2021-03-31 10:20:48 -0300

    Contract creation seems to be ok.

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,21 @@
+const Example = artifacts.require("./Example.sol");
+
+// If you want to hardcode what deploys, comment out process.env.X and use
+// true/false;
+const DEPLOY_EXAMPLE = process.env.DEPLOY_EXAMPLE;
+
+module.exports = async (deployer, network, addresses) => {
+    // OpenSea proxy registry addresses for rinkeby and mainnet.
+    let proxyRegistryAddress = "";
+    if (network === "rinkeby") {
+        proxyRegistryAddress = "0xf57b2c51ded3a29e6891aba85459d600256cf317";
+    } else {
+        proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
+    }
+
+    if (DEPLOY_EXAMPLE) {
+        await deployer.deploy(Example, proxyRegistryAddress, {
+            gas: 5000000
+        });
+    }
+};
